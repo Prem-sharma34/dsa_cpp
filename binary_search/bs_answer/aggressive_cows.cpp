f225ecb6d7b158e1d3fce073525ebc0ff7620fa5
Prem-sharma34 <ps4183085@gmail.com>
2026-02-03 00:00:41 +0530

    updated problem

--- binary_search/bs_answer/aggressive_cows.cpp
+++ binary_search/bs_answer/aggressive_cows.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -10,9 +11,31 @@ bool canweplace(vector<int>&arr , int distance , int cows)
 }
 
 
+int aggressiveCows(vector<int> &arr, int k) 
+{
+    sort(arr.begin() , arr.end());
+    int n = arr.size();
+    int low  = 0 , high = arr[n-1] - arr[0];
+
+    while(low <= high)
+    {
+        int mid = low+high/2;
+
+        if(canweplace(arr,mid,k) == true)
+        {
+            
+        }else
+        {
+           
+        }
+    }
+}
+
 
 
 int main()
 {
-    
+    vector<int> arr = {0, 3, 4, 7, 10, 9};
+    int k =3;
+    cout<<aggressiveCows(arr , k);
 }
