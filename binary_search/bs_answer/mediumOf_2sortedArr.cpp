50ebaec02683ae1aab06682a93f5eaba8d3eb699
Prem-sharma34 <ps4183085@gmail.com>
2026-02-05 23:29:23 +0530

    optimal solution for 2 medium problem

--- binary_search/bs_answer/mediumOf_2sortedArr.cpp
+++ binary_search/bs_answer/mediumOf_2sortedArr.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+#include<limits.h>
 
 using namespace std;
 
@@ -42,26 +43,43 @@ vector<int> merge(vector<int>& arr1, vector<int>& arr2)
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
 {
-    int m = nums1.size();
-    int n = nums2.size();
+    int n1 = nums1.size();
+    int n2 = nums2.size();
         
-    vector<int>arr = merge(nums1,nums2);
+    if(n1 > n2) return findMedianSortedArrays(nums2 , nums1);
 
-    int total = arr.size();
+    int low = 0 , high = n1;
 
-    if(total%2 == 1)
+    int left = (n1 + n2 +1)/2;
+
+    int n = n1 + n2;
+
+    while(low <= high)
     {
-        return (double)arr[total/2];
-    }else
-    {
-        int med2= total/2;
-        int med1 = med2-1;
+        int mid1 = (low + high)>>1;
+        int mid2 = left-mid1;
 
-        double result = (arr[med1] + arr[med2])/2.0;
-        return result;
+        
+    int l1 = INT_MIN , l2 = INT_MIN;
+    int r1 = INT_MAX , r2 = INT_MAX;
+
+    if(mid1 < n1) r1 = nums1[mid1];
+    if(mid2 < n2) r2 = nums2[mid2];
+
+    if(mid1 -1 >= 0) l1 = nums1[mid1-1];
+    if(mid2-1>= 0) l2 = nums2[mid2-1];
+
+    if(l1<= r2 && l2 <= r1) 
+    {
+        if(n%2==1) return max(l1 , l2);
+        return (double)(max(l1,l2) + min(r1,r2))/2.0;
+    }
+    else if(l1 > r2) high = mid1 -1;
+    else low = mid1+1;
+
     }
 
-    return 1;
+    return 0;
 
 }
 
