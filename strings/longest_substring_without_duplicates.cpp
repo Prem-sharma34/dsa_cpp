2769c3517f36460761d7f46c5e279e9155f50c39
Prem-sharma34 <ps4183085@gmail.com>
2026-02-16 23:56:21 +0530

    start learning sliding windows approach

--- strings/longest_substring_without_duplicates.cpp
+++ strings/longest_substring_without_duplicates.cpp
@@ -9,34 +9,10 @@ public:
     int lengthOfLongestSubstring(string s)
     {
 
-        int n = s.length();
+        
 
-        if (n < 1)
-            return 0;
-        else if (n == 1)
-            return 1;
 
-        unordered_map<char, int> mpp;
-        int len = 0;
-        for(int i = 0 ; i < n ; i++)
-        {
-
-            for(int j = i ; j < n ; j++)
-            {
-                if(mpp[s[j]] == 1) break;
-
-                mpp[s[j]] =1 ;
-
-                int l = j-i +1;
-
-                len = max(len,l);
-            }
-            mpp.clear();
-           
-        }
-       
-
-        return len;
+     
     }
 };
 
