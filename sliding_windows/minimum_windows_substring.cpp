7dc9417f83a853bfa4ecb45ac24826da1f8c90ea
Prem-sharma34 <ps4183085@gmail.com>
2026-02-22 00:52:51 +0530

    solved minimum windows substring t<<2hr

---
+++ sliding_windows/minimum_windows_substring.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<limits.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+           int n = s.size();
+           int  m = t.size();
+
+           unordered_map<char , int>hash;
+           for(auto i : t) hash[i]++;
+           int l = 0 , r = 0 , cnt = 0;
+           int minlen = INT_MAX;
+           int startindex = -1;
+
+           while(r < n)
+           {
+                if(hash[s[r]] > 0) cnt++;
+                hash[s[r]]--;
+
+                while(cnt == m)
+                {
+                    if(r - l +1 < minlen)
+                    {
+                        minlen = r -l +1;
+                        startindex = l;
+                    }
+
+                    hash[s[l]]++;
+                    if(hash[s[l]] > 0) cnt--;
+                    l++;
+
+                }
+
+                r++;
+           }
+
+           return startindex == -1 ? "" : s.substr(startindex , minlen);
+    }
+};
+
+
+
+int main()
+{
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    Solution sol;
+
+    cout<<sol.minWindow(s , t)<<endl;
+}
